3dcdbd1b3f8de79496138d5ee483ad47bb3f43b7
vipex <101529155+vipexv@users.noreply.github.com>
2024-10-18 04:09:28 +0200

    ci: update packages/theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,8 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"sainnhe/sonokai",
 	config = function()
-		require("gruvbox").setup({
-			transparent_mode = true,
-		})
-
-		vim.cmd("colorscheme gruvbox")
+		vim.g.sonokai_enable_italic = true
+		vim.g.sonokai_transparent_background = 1
+		vim.cmd("colorscheme sonokai")
 	end,
 }
