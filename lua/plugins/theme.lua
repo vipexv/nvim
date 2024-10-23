6ed7cbfb0b18f5ed8b59973fd775d69e0fbfc77f
vipex <101529155+vipexv@users.noreply.github.com>
2024-10-23 03:11:57 +0200

    ci: make background transparent when not using neovide

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,7 +2,10 @@ return {
 	"sainnhe/sonokai",
 	config = function()
 		vim.g.sonokai_enable_italic = true
-		-- vim.g.sonokai_transparent_background = 1
+
+		if not vim.g.neovide then
+			vim.g.sonokai_transparent_background = 1
+		end
 		vim.cmd("colorscheme sonokai")
 	end,
 }
