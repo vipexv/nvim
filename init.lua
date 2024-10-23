f415c0e3f47bb40c6cc6e47babad3c71da66f8df
vipex <101529155+vipexv@users.noreply.github.com>
2024-10-23 03:08:19 +0200

    ci: update color scheme and add config for neovide

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
 require("vipex")
+
+if vim.g.neovide then
+	vim.o.guifont = "Iosevka Nerd Font:h15"
+	vim.g.neovide_hide_mouse_when_typing = false
+	vim.g.neovide_confirm_quit = false
+	vim.g.neovide_fullscreen = true
+end
