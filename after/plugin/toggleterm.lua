92152b813dcd2480b62231f5dfb11bb8ac861fd7
vipexv <101529155+vipexv@users.noreply.github.com>
2024-11-05 17:50:38 +0100

    ci: remove toggle term and update packages

--- after/plugin/toggleterm.lua
+++
@@ -1,7 +0,0 @@
-require("toggleterm").setup({
-	direction = "float",
-	open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
-	float_opts = {
-		border = "curved",
-	},
-})
