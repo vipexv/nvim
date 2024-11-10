5f98c0a9f4874e9f732b8dc213e8ab7adac56dbb
vipexv <101529155+vipexv@users.noreply.github.com>
2024-11-10 04:32:05 +0100

    ci: remove stoopid treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,5 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPost", "BufNewFile" }, -- More specific than VeryLazy
+	event = { "BufReadPost", "BufNewFile" },
 	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "vim", "vimdoc" }, -- Add your commonly used languages
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
 }
