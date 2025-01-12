0ed02ad3dc97e625aa8a59dd90258e3a96f9a6b7
vipexv <101529155+vipexv@users.noreply.github.com>
2025-01-12 02:36:17 +0100

    ci: update packages, add more `ensure_installed` languages by default

--- lua/vipex/init.lua
+++ lua/vipex/init.lua
@@ -30,19 +30,9 @@ vim.g.maplocalleader = ","
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
-		{
-			"neovim/nvim-lspconfig",
-			dependencies = {
-				"mason.nvim",
-				"mason-lspconfig.nvim",
-			},
-		},
-		{
-			"williamboman/mason.nvim",
-		},
-		{
-			"williamboman/mason-lspconfig.nvim",
-		},
+		{ "neovim/nvim-lspconfig", dependencies = { "mason.nvim", "mason-lspconfig.nvim" } },
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
 		{
 			"hrsh7th/nvim-cmp",
 			dependencies = {
@@ -53,7 +43,16 @@ require("lazy").setup({
 			},
 		},
 	},
-	install = {},
+
+	defaults = {
+		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+		lazy = true,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
 	performance = {
 		cache = {
 			enabled = true,
@@ -63,8 +62,6 @@ require("lazy").setup({
 			reset = true,
 			disabled_plugins = {
 				"gzip",
-				"matchit",
-				"matchparen",
 				"tarPlugin",
 				"tohtml",
 				"tutor",
