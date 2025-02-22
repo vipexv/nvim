6ad3be373355ea04845f8a525abedf2109dbb83b
vipexv <101529155+vipexv@users.noreply.github.com>
2025-02-22 22:32:50 +0100

    chore: update packages

--- fivem-library/ox-types/ox_lib/ox_lib/resource/zoneCreator/server.lua
+++ fivem-library/ox-types/ox_lib/ox_lib/resource/zoneCreator/server.lua
@@ -41,36 +41,36 @@ local parse = {
 	poly = function(data)
 		local points = {}
 		for i = 1, #data.points do
-			points[#points + 1] = ('\t\tvec3(%s, %s, %s),\n'):format((data.points[i].x), (data.points[i].y), data.zCoord)
+			points[#points + 1] = ("\t\tvec3(%s, %s, %s),\n"):format(data.points[i].x, data.points[i].y, data.zCoord)
 		end
 
 		local pattern
-		if data.format == 'function' then
+		if data.format == "function" then
 			pattern = {
-				'local poly = lib.zones.poly({\n',
+				"local poly = lib.zones.poly({\n",
 				('\tname = "%s",\n'):format(data.name),
-				'\tpoints = {\n',
-				('%s\t},\n'):format(table.concat(points)),
-				('\tthickness = %s,\n'):format(data.height),
-				'})\n',
+				"\tpoints = {\n",
+				("%s\t},\n"):format(table.concat(points)),
+				("\tthickness = %s,\n"):format(data.height),
+				"})\n",
 			}
-		elseif data.format == 'array' then
+		elseif data.format == "array" then
 			pattern = {
-				'{\n',
+				"{\n",
 				('\tname = "%s",\n'):format(data.name),
-				'\tpoints = {\n',
-				('%s\t},\n'):format(table.concat(points)),
-				('\tthickness = %s,\n'):format(data.height),
-				'},\n'
+				"\tpoints = {\n",
+				("%s\t},\n"):format(table.concat(points)),
+				("\tthickness = %s,\n"):format(data.height),
+				"},\n",
 			}
-		elseif data.format == 'target' then
+		elseif data.format == "target" then
 			pattern = {
-				'exports.ox_target:addPolyZone({\n',
+				"exports.ox_target:addPolyZone({\n",
 				('\tname = "%s",\n'):format(data.name),
-				'\tpoints = {\n',
-				('%s\t},\n'):format(table.concat(points)),
-				('\tthickness = %s,\n'):format(data.height),
-				'})\n'
+				"\tpoints = {\n",
+				("%s\t},\n"):format(table.concat(points)),
+				("\tthickness = %s,\n"):format(data.height),
+				"})\n",
 			}
 		end
 
@@ -78,32 +78,32 @@ local parse = {
 	end,
 	box = function(data)
 		local pattern
-		if data.format == 'function' then
+		if data.format == "function" then
 			pattern = {
-				'local box = lib.zones.box({\n',
+				"local box = lib.zones.box({\n",
 				('\tname = "%s",\n'):format(data.name),
-				('\tcoords = vec3(%s, %s, %s),\n'):format(data.xCoord, data.yCoord, data.zCoord),
-				('\tsize = vec3(%s, %s, %s),\n'):format(data.width, data.length, data.height),
-				('\trotation = %s,\n'):format(data.heading),
-				'})\n',
+				("\tcoords = vec3(%s, %s, %s),\n"):format(data.xCoord, data.yCoord, data.zCoord),
+				("\tsize = vec3(%s, %s, %s),\n"):format(data.width, data.length, data.height),
+				("\trotation = %s,\n"):format(data.heading),
+				"})\n",
 			}
-		elseif data.format == 'array' then
+		elseif data.format == "array" then
 			pattern = {
-				'{\n',
+				"{\n",
 				('\tname = "%s",\n'):format(data.name),
-				('\tcoords = vec3(%s, %s, %s),\n'):format(data.xCoord, data.yCoord, data.zCoord),
-				('\tsize = vec3(%s, %s, %s),\n'):format(data.width, data.length, data.height),
-				('\trotation = %s,\n'):format(data.heading),
-				'},\n',
+				("\tcoords = vec3(%s, %s, %s),\n"):format(data.xCoord, data.yCoord, data.zCoord),
+				("\tsize = vec3(%s, %s, %s),\n"):format(data.width, data.length, data.height),
+				("\trotation = %s,\n"):format(data.heading),
+				"},\n",
 			}
-		elseif data.format == 'target' then
+		elseif data.format == "target" then
 			pattern = {
-				'exports.ox_target:addBoxZone({\n',
+				"exports.ox_target:addBoxZone({\n",
 				('\tname = "%s",\n'):format(data.name),
-				('\tcoords = vec3(%s, %s, %s),\n'):format(data.xCoord, data.yCoord, data.zCoord),
-				('\tsize = vec3(%s, %s, %s),\n'):format(data.width, data.length, data.height),
-				('\trotation = %s,\n'):format(data.heading),
-				'})\n',
+				("\tcoords = vec3(%s, %s, %s),\n"):format(data.xCoord, data.yCoord, data.zCoord),
+				("\tsize = vec3(%s, %s, %s),\n"):format(data.width, data.length, data.height),
+				("\trotation = %s,\n"):format(data.heading),
+				"})\n",
 			}
 		end
 
@@ -111,29 +111,29 @@ local parse = {
 	end,
 	sphere = function(data)
 		local pattern
-		if data.format == 'function' then
+		if data.format == "function" then
 			pattern = {
-				'local sphere = lib.zones.sphere({\n',
+				"local sphere = lib.zones.sphere({\n",
 				('\tname = "%s",\n'):format(data.name),
-				('\tcoords = vec3(%s, %s, %s),\n'):format(data.xCoord, data.yCoord, data.zCoord),
-				('\tradius = %s,\n'):format(data.height),
-				'})\n',
+				("\tcoords = vec3(%s, %s, %s),\n"):format(data.xCoord, data.yCoord, data.zCoord),
+				("\tradius = %s,\n"):format(data.height),
+				"})\n",
 			}
-		elseif data.format == 'array' then
+		elseif data.format == "array" then
 			pattern = {
-				'{\n',
+				"{\n",
 				('\tname = "%s",\n'):format(data.name),
-				('\tcoords = vec3(%s, %s, %s),\n'):format(data.xCoord, data.yCoord, data.zCoord),
-				('\tradius = %s,\n'):format(data.height),
-				'},\n',
+				("\tcoords = vec3(%s, %s, %s),\n"):format(data.xCoord, data.yCoord, data.zCoord),
+				("\tradius = %s,\n"):format(data.height),
+				"},\n",
 			}
-		elseif data.format == 'target' then
+		elseif data.format == "target" then
 			pattern = {
-				'exports.ox_target:addSphereZone({\n',
+				"exports.ox_target:addSphereZone({\n",
 				('\tname = "%s",\n'):format(data.name),
-				('\tcoords = vec3(%s, %s, %s),\n'):format(data.xCoord, data.yCoord, data.zCoord),
-				('\tradius = %s,\n'):format(data.height),
-				'})\n',
+				("\tcoords = vec3(%s, %s, %s),\n"):format(data.xCoord, data.yCoord, data.zCoord),
+				("\tradius = %s,\n"):format(data.height),
+				"})\n",
 			}
 		end
 
@@ -141,8 +141,10 @@ local parse = {
 	end,
 }
 
-RegisterNetEvent('ox_lib:saveZone', function(data)
-    if not source or not IsPlayerAceAllowed(source, 'command') then return end
-    local output = (LoadResourceFile(cache.resource, 'created_zones.lua') or '') .. parse[data.zoneType](data)
-    SaveResourceFile(cache.resource, 'created_zones.lua', output, -1)
+RegisterNetEvent("ox_lib:saveZone", function(data)
+	if not source or not IsPlayerAceAllowed(source, "command") then
+		return
+	end
+	local output = (LoadResourceFile(cache.resource, "created_zones.lua") or "") .. parse[data.zoneType](data)
+	SaveResourceFile(cache.resource, "created_zones.lua", output, -1)
 end)
