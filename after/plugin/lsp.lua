3fef5c479214ee1b72e36c42a3c31d840280123e
vipexv <101529155+vipexv@users.noreply.github.com>
2025-04-22 15:02:54 +0200

    fix(lsp/tailwind): lsp not launching

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -151,7 +151,39 @@ require("mason-lspconfig").setup({
                 },
             })
         end,
-
+        tailwindcss = function()
+            require("lspconfig").tailwindcss.setup({
+                filetypes = {
+                    "css",
+                    "scss",
+                    "sass",
+                    "postcss",
+                    "html",
+                    "javascript",
+                    "javascriptreact",
+                    "typescript",
+                    "typescriptreact",
+                    "svelte",
+                    "vue",
+                    "rust",
+                    -- "rs",
+                },
+                experimental = {
+                    classRegex = {
+                        [[class="([^"]*)]],
+                        'class=\\s+"([^"]*)',
+                    },
+                },
+                root_dir = require("lspconfig").util.root_pattern(
+                    "tailwind.config.js",
+                    "tailwind.config.ts",
+                    "postcss.config.js",
+                    "postcss.config.ts",
+                    "package.json",
+                    "node_modules"
+                ),
+            })
+        end,
         svelte = function()
             require("lspconfig").svelte.setup({
                 capabilities = lspCapabilities,
